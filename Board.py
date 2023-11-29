8fe50646d95ed7bbebb9b0021caf192366de943b
the-mr-poarbear <arsam.amr@gmail.com>
2023-11-29 22:30:05 +0330

    sorry classes again

---
+++ Board.py
@@ -0,0 +1,9 @@
+class Board:
+    
+    def getPieceOnGivenSquare(self , picFile , picRank) :
+        #file is a to h and rank is 1 to 8
+        pass 
+
+
+     
+
---
+++ Piece.py
@@ -0,0 +1,14 @@
+
+
+class Piece:
+    
+    def __init__(self , tag ) :
+        pass
+
+    def Move(self ) :
+        pass 
+    
+    def DeletePiece(self) :
+        pass
+
+    
